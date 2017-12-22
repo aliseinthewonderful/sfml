22e3e410ff8d6f83039432581c5379376f6a5602
aliseinthewonderful <34268034+aliseinthewonderful@users.noreply.github.com>
2017-12-23 01:21:17 +0300

    Add files via upload

--- sfml.3/03/main.cpp
+++ sfml.3/03/main.cpp
@@ -1,35 +1,37 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <iostream>
+#include <algorithm>
 
 float toDegrees(float radians)
 {
     return float(double(radians) * 180.0 / M_PI);
 }
 
+//Обрабатывает событие mousemove, обновляя позицию мыши
 void onMouseMove(const sf::Event::MouseMoveEvent& event, sf::Vector2f& mousePosition)
 {
     std::cout << "mouse x=" << event.x << ", y=" << event.y << std::endl;
     mousePosition = { float(event.x), float(event.y) };
-
 }
 
 void init(sf::ConvexShape& pointer)
 {
-    pointer.setPointCount(3);
-    pointer.setPoint(0, { 40, 0 });
-    pointer.setPoint(1, { -20, -20 });
-    pointer.setPoint(2, { -20, 20 });
-    pointer.setPosition({ 400, 300 });
-    pointer.setFillColor(sf::Color(0xFF, 0x80, 0x00));
+   pointer.setPointCount(3);
+   pointer.setPoint(0, { 40, 0 });
+   pointer.setPoint(1, { -20, -20 });
+   pointer.setPoint(2, { -20, 20 });
+   pointer.setPosition({ 400, 300 });
+   pointer.setFillColor(sf::Color(0xFF, 0x80, 0x00));
+    
 }
 
 void pollEvents(sf::RenderWindow& window, sf::Vector2f& mousePosition)
 {
     sf::Event event;
-    while(window.pollEvent(event))
+    while (window.pollEvent(event))
     {
-        switch(event.type)
+        switch (event.type)
         {
             case sf::Event::Closed:
                 window.close();
@@ -41,45 +43,50 @@ void pollEvents(sf::RenderWindow& window, sf::Vector2f& mousePosition)
                 break;
         }
     }
+   
 }
 
-void update(const sf::Vector2f& mousePosition, sf::ConvexShape& pointer, float deltaTime)
+void update(const sf::Vector2f& mousePosition, sf::ConvexShape& pointer,float& dt)
 {
-  const sf::Vector2f delta = mousePosition - pointer.getPosition();
-  float angle = atan2(delta.y, delta.x);
-  if (angle < 0)
-  {
-      angle = angle + 2 * M_PI;
-  }
-  angle = toDegrees(angle); 
-  const float maxRotation = 15 * deltaTime; 
-  const float rotation = std::abs((angle - pointer.getRotation())); 
-  if (angle != pointer.getRotation()) 
-  {
-    if (angle > pointer.getRotation()) 
-  {
-      if ((angle - 180) > pointer.getRotation()) 
+    const sf::Vector2f delta = mousePosition - pointer.getPosition();
+    float angle = atan2(delta.y, delta.x);
+    if (angle <0)
+    {   
+        angle = angle + 2 * M_PI;
+        
+    }
+    angle = toDegrees(angle); 
+    const float maxRotation = 15 * dt; 
+    const float rotation = std::abs((angle - pointer.getRotation())); 
+    if (angle != pointer.getRotation()) 
     { 
-        pointer.setRotation(pointer.getRotation() - std::min(maxRotation, rotation)); 
-    } 
+        if (angle > pointer.getRotation()) 
+    { 
+        if ((angle - 180) > pointer.getRotation()) 
+        { 
+            pointer.setRotation(pointer.getRotation() - std::min(maxRotation, rotation)); 
+        } 
             else 
         { 
             pointer.setRotation(pointer.getRotation() + std::min(maxRotation, rotation)); 
         } 
-  } 
-        else 
-    { 
-        if ((angle + 180) < pointer.getRotation()) 
-        { 
-            pointer.setRotation(pointer.getRotation() + std::min(maxRotation, rotation)); 
-        } 
+    } 
         else 
         { 
-            pointer.setRotation(pointer.getRotation() - std::min(maxRotation, rotation)); 
+             if ((angle + 180) < pointer.getRotation()) 
+            {        
+                pointer.setRotation(pointer.getRotation() + std::min(maxRotation, rotation)); 
+            } 
+                else 
+            {
+                pointer.setRotation(pointer.getRotation() - std::min(maxRotation, rotation)); 
+            } 
         } 
     } 
-  } 
 } 
+    
+  
+  
 
   
 
@@ -101,8 +108,9 @@ int main()
     sf::RenderWindow window(
         sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT}),
         "Pointer follows mouse with max speed 15 degrees per second", sf::Style::Default, settings);
-        window.setFramerateLimit(60);
-        sf::Clock clock;
+        window.setFramerateLimit(60); 
+        sf::Clock clock; 
+        
     sf::ConvexShape pointer;
     sf::Vector2f mousePosition;
     init(pointer);
